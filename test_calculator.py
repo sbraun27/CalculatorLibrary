5edf270b2118dcf36a1fe94446c031e99a12890f
sbraun27 <sbraun27@gmail.com>
2021-01-21 19:36:28 -0700

    Adding more tests.

--- test_calculator.py
+++ test_calculator.py
@@ -12,3 +12,9 @@ class TestCalculator:
 
     def test_subtraction(self):
         assert 2 == calculator.subtract(4, 2)
+
+    def test_multiplication(self):
+        assert 8 == calculator.multiplication(4, 2)
+
+    def test_division(self):
+        assert 10 == calculator.division(100, 10)
